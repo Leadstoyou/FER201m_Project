f17342aec859d38346a7fdbf18720e6677e0b9cb
Leadstoyou <lytieulong2j2@gmail.com>
2023-03-22 23:07:27 +0700

    11:07

--- src/components/home.js
+++ src/components/home.js
@@ -2,63 +2,60 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import "./home.css";
-
+import categories from "../Categories.json";
 const Home = () => {
+  const [films, setfilms] = useState(JSON.parse(localStorage.getItem("films")));
+  const [listCategory, setlistCategory] = useState(categories);
+  console.log(films, listCategory);
   return (
     <div>
       <Navbar />
       <div className="home">
-			<div className="home_wrap">
-				<div className="albums_category">
-					<div className="albums_title"> TYPE
-                    <ul>
-                        <li><a href="">Action</a></li>
-                        <li><a href="">Cartoon</a></li>
-                        <li><a href="">Horror</a></li>
-                        <li><a href="">Science-Art</a></li>
-                        <li><a href="">Romantic</a></li>
-                    </ul>
-					</div>
-				</div>
-				<div className="albums_items">
-					<div className="albums_items_wrap" style={{border: "1px solid #ccc", marginRight: "30px"}}>
-						<img style={{width: "100%", height: "300px", objectFit: "cover"}} src="https://upload.wikimedia.org/wikipedia/vi/a/ab/Titanic_3D_poster_Vietnam.jpg"/>
-                        <div className="album_des"><h5>Name:</h5> </div>
-                        <div className="album_des"><h5>Type:</h5></div>
-                        <div className="album_des"><h5>Score:</h5> </div>
-                        <button className="album_but">Comment</button>
-					</div>
-                    <div className="albums_items_wrap" style={{border: "1px solid #ccc", marginRight: "30px"}}>
-						<img style={{width: "100%", height: "300px", objectFit: "cover"}} src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg"/>
-                        <div className="album_des"><h5>Name:</h5> </div>
-                        <div className="album_des"><h5>Type:</h5></div>
-                        <div className="album_des"><h5>Score:</h5> </div>
-                        <button className="album_but">Comment</button>
-					</div>
-                    <div className="albums_items_wrap" style={{border: "1px solid #ccc", marginRight: "30px"}}>
-						<img style={{width: "100%", height: "300px", objectFit: "cover"}} src="https://cdn.gamerjournalist.com/primary/2021/01/Genshin-Impact-Ganyu-Talents-Constellations-and-Ascension-Materials.jpg"/>
-                        <div className="album_des"><h5>Name:</h5> </div>
-                        <div className="album_des"><h5>Type:</h5></div>
-                        <div className="album_des"><h5>Score:</h5> </div>
-                        <button className="album_but">Comment</button>
-					</div>
-                    <div className="albums_items_wrap" style={{border: "1px solid #ccc", marginRight: "30px"}}>
-						<img style={{width: "100%", height: "300px", objectFit: "cover"}} src="https://cdn.gamerjournalist.com/primary/2021/01/Genshin-Impact-Ganyu-Talents-Constellations-and-Ascension-Materials.jpg"/>
-                        <div className="album_des"><h5>Name:</h5> </div>
-                        <div className="album_des"><h5>Type:</h5></div>
-                        <div className="album_des"><h5>Score:</h5> </div>
-                        <button className="album_but">Comment</button>
-					</div>
-                    <div className="albums_items_wrap" style={{border: "1px solid #ccc", marginRight: "30px"}}>
-						<img style={{width: "100%", height: "300px", objectFit: "cover"}} src="https://cdn.gamerjournalist.com/primary/2021/01/Genshin-Impact-Ganyu-Talents-Constellations-and-Ascension-Materials.jpg"/>
-                        <div className="album_des"><h5>Name:</h5> </div>
-                        <div className="album_des"><h5>Type:</h5></div>
-                        <div className="album_des"><h5>Score:</h5> </div>
-                        <button className="album_but">Comment</button>
-					</div>
-				</div>
-			</div>
-		</div>
+        <div className="home_wrap">
+          <div className="albums_category">
+            <div className="albums_title">
+              <ul>
+                {listCategory.map((category, index) => (
+                  <li key={index}>
+                    <a href={`/home/category${category.id}`}>{category.category}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="album_items_container">
+            <div className="albums_items">
+              {films.map((film, index) => (
+                <div
+                  className="albums_items_wrap"
+                  style={{ border: "1px solid #ccc", marginRight: "30px" }}
+                  key={index}
+                >
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "300px",
+                      objectFit: "cover",
+                      alt:`Picture #${index}`
+                    }}
+                    src={film.imageUrl}
+                  />
+                  <div className="album_des">
+                    <h5>Name:</h5>{film.name}
+                  </div>
+                  <div className="album_des">
+                    <h5>Type:</h5>{film.category}
+                  </div>
+                  <div className="album_des">
+                    <h5>Score:</h5>{film.score}
+                  </div>
+                  <button className="album_but">Comment</button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
