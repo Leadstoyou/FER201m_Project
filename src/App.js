79b15778828ee9c68f352a4460ada8bdafff76ac
lqt <lqt@gmail.com>
2023-03-22 18:11:06 +0700

    add commentPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './components/login'
 import Register from './components/register';
+import CommentPage from './components/commentPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/comment" element={<CommentPage />} />
       </Routes>
     </Router>
   );
