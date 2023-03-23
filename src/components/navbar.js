54005723c1a4137656267c1c6a12cafaf8b9a762
Leadstoyou <lytieulong2j2@gmail.com>
2023-03-23 08:42:02 +0700

    lmeolmeo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import "./navbar.css";
 
 function Navbar() {
   const [searchItem, setSearchItem] = useState("");
-
+  const loginedUser = JSON.parse(localStorage.getItem("UserID"));
+  const navigation = useNavigate();
   const handleSearch = () => {
     const encodedSearchItem = encodeURIComponent(searchItem.trim());
     if (encodedSearchItem) {
       window.location.href = `/home?search=${encodedSearchItem}`;
     }
   };
-
+  const handleLogout = () => {
+    localStorage.removeItem("UserID");
+    navigation("/");
+  }
+  console.log(loginedUser);
   return (
     <nav className="navbar">
       <div className="nav-menu">
@@ -21,19 +27,28 @@ function Navbar() {
           </a>
         </div>
 
-        <div className="nav-item right">
-          <input
-            placeholder="Nhập phim cần tìm"
-            onChange={(e) => setSearchItem(e.target.value)}
-          ></input>
-          <button onClick={() => handleSearch()}>Tìm</button>
-          <a href="./" className="nav-link">
-            Đăng nhập
-          </a>
-          <a href="./register" className="nav-link">
-            Đăng ký
-          </a>
-        </div>
+        {loginedUser ? (
+          <div className="nav-item right">
+            <span>Chào {loginedUser.name}!</span>
+            <button onClick={() => handleLogout()}>
+              Đăng xuất
+            </button>
+          </div>
+        ) : (
+          <div className="nav-item right">
+            <input
+              placeholder="Nhập phim cần tìm"
+              onChange={(e) => setSearchItem(e.target.value)}
+            ></input>
+            <button onClick={() => handleSearch()}>Tìm</button>
+            <a href="./" className="nav-link">
+              Đăng nhập
+            </a>
+            <a href="./register" className="nav-link">
+              Đăng ký
+            </a>
+          </div>
+        )}
       </div>
     </nav>
   );
